a134976d3c49c4d5d9831c94c4cc17ddab5f0266
Georgy Voskanyan <hello@voskanyan.dev>
2024-09-18 12:36:03 +0200

    Add classes from n1e1

---
+++ src/n1e1/Instrument.java
@@ -0,0 +1,17 @@
+package n1e1;
+
+public abstract class Instrument {
+    abstract public void play();
+    private final String name;
+    private final double price;
+
+    public Instrument(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void printInfo() {
+        System.out.println("L'instrument " + name + " te el preu de " + price + ".");
+    }
+}
+
---
+++ src/n1e1/WindInstrument.java
@@ -0,0 +1,14 @@
+package n1e1;
+
+public class WindInstrument extends Instrument {
+    static {
+        System.out.println("La classe WindInstrument s'està carregant");
+    }
+    public WindInstrument(String name, double price) {
+        super(name, price);
+    }
+
+    public void play() {
+        System.out.println("Està sonant un instrument de vent");
+    }
+}
